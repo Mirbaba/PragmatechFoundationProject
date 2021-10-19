97064845b2fa5ac20dd9f3597335db63195a0123
Mirbaba Aliyev <aliyevmirbaba@gmail.com>
2021-10-19 20:38:17 +0400

    Task#2

---
+++ Algorithms/algo_2/java.js
@@ -0,0 +1,12 @@
+let a=prompt("What's your name?");
+alert(a);
+let b=alert(a);
+let c=alert(a);
+let d=alert(a);
+let e=alert(a);
+let f=alert(a);
+let g=alert(a);
+let h=alert(a);
+let j=alert(a);
+let k=alert(a);
+
